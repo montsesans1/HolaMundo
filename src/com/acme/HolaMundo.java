d4938630757c4798a2b9ea5b43c0c8651cdd5bce
montsesans1 <montsesans1@gmail.com>
2024-02-29 16:25:23 +0100

    Nuevo codigo HolaMundo en main

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -2,6 +2,12 @@ package com.acme;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.print("Hola Mundo!!!");
+        System.out.print("Hola Mundo!!!\n");
+        Mensaje msg = new Mensaje();
+        String nombre = msg.solicitaDatos("Entre su nombre: ");
+        String apellido = msg.solicitaDatos("Entre su apellido: ");
+        Persona p1 = new Persona(nombre, apellido);
+        //System.out.println("Bienvenido " + nombre + " " + apellido);
+        msg.saludar(p1);
     }
 }
