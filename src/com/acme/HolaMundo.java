8f7328a03148dacbf85b73c38798b99edad0815b
montsesans1 <montsesans1@gmail.com>
2024-02-29 14:19:25 +0100

    Se agregó un mensaje inservible

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -1,7 +1,10 @@
 package com.acme;
 
+import java.sql.SQLOutput;
+
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.print("Hola Mundo 2!");
+        System.out.println("Esta instrucción es inservible");
     }
 }
