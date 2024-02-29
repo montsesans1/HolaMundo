7cff9685f19acea9960eb9eda6d8ce5998ccf3e5
montsesans1 <montsesans1@gmail.com>
2024-02-29 09:13:36 +0100

    Se integró la clase Mensaje

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -2,6 +2,8 @@ package com.acme;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.print("Hola Mundo!!");
+        Mensaje msg = new Mensaje();
+        String nombre = msg.solicitaDatos("Escribe tu nombre: ");
+        msg.saludar(nombre);
     }
 }
